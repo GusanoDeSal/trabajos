939a371a746b535f3cd563fe37bd9be8073bc09f
Miquel <Miquel@Miquel-PC>
2014-05-02 20:20:03 +0200

    Hecha la clase de excepciones

---
+++ APISql/src/iesPaucassesNoves/cat/SqlDBException.java
@@ -0,0 +1,14 @@
+package iesPaucassesNoves.cat;
+
+public class SqlDBException extends Exception{
+
+	public SqlDBException(){
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public SqlDBException(String message){
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
